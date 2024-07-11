cf9e3e35ee0af2e5e7edf0ccd0aff5af4c8a0d93
RenanRSilva <renan.rambul@takeandgoapp.com>
2024-07-11 16:19:46 -0300

    refactor: remove main function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 
 #![no_std]
+#![no_main]
 
-use core::panic::PanicInfo
+use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn main() {}
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {};
+}
+
